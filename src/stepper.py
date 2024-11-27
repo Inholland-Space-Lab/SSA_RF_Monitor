d6a4489caa60372fdd2c2a9539edf4b216a37bdf
Lord_Redst0ne <74739340+enemieltje@users.noreply.github.com>
2024-11-27 15:01:04 +0100

    comments

--- src/stepper.py
+++ src/stepper.py
@@ -20,24 +20,7 @@ def line(i):
     return UP*i + CLR
 
 
-class Direction():
-    clockwise = GPIO.HIGH
-    counter_clockwise = GPIO.LOW
-
-    def flip(direction):
-        if direction == Direction.clockwise:
-            return Direction.counter_clockwise
-        elif direction == Direction.counter_clockwise:
-            return Direction.clockwise
-
-
-class StepMode():
-    linear = 0
-    exp = 1
-
-
 class Stepper():
-    # step_pin: int
     dir_pin: int
     enable_pin: int
     # position: int
@@ -57,6 +40,7 @@ class Stepper():
     acceleration: float  # steps per second^2
     goal: int  # steps
 
+    # get the position indicated by the sensor using the position_callback defined in Dish._setup_motors
     @property
     def sensor_position(self):
         try:
@@ -65,11 +49,13 @@ class Stepper():
                 return pos
             else:
                 return 0
-        except:
+        except Exception as e:
             logger.warning(f"position callback not implemented!")
+            logger.error(str(e))
             self.stop_pid()
             return 0
 
+    # get the distance from the current position to the goal
     @property
     def distance(self) -> int:
         position = self.sensor_position / 360 * self.steps_per_rev
@@ -78,10 +64,12 @@ class Stepper():
             distance -= self.steps_per_rev
         return distance
 
+    # get the amount of steps required for one full revolution
     @property
     def steps_per_rev(self):
         return self.resolution * self.gear_ratio
 
+    #
     def __str__(self):
         return \
             f"a: {self.acceleration:.4f}\n" + \
@@ -94,7 +82,6 @@ class Stepper():
 
     def __init__(
         self,
-        # step_pin,
         dir_pin,
         enable_pin,
         pwm,
@@ -102,16 +89,18 @@ class Stepper():
         resolution=3200,
         gear_ratio=(19+(38/187)),
     ):
-        # Define Pins
         logger.info("Creating Stepper")
-        # self.step_pin = step_pin
+
+        # Define Pins
         self.dir_pin = dir_pin
         self.enable_pin = enable_pin
+        self.pwm = pwm
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup([dir_pin, enable_pin], GPIO.OUT)
+
+        # Motor properties
         self.gear_ratio = gear_ratio
         self.resolution = resolution
-        # self.position = 0
-        self.pwm = pwm
-        self.do_pid = False
 
         # Setup the thread and queue that will run the motor
         self.job_queue = queue.Queue()
@@ -119,10 +108,11 @@ class Stepper():
         self.worker_thread.daemon = True  # Ensure it exits when the main thread exits
         self.worker_thread.start()
 
+        # Setup PID properties
+        self.do_pid = False
         self.max_acceleration = 2000
         self.max_velocity = 100000
         self.position_callback = position_callback
-        # self.sensor = sensor
         self.acceleration = 0
         self.velocity = 0
         self.goal = 0
@@ -130,10 +120,6 @@ class Stepper():
         self._last_time = 0
         self.pid = PID(0, 0, 0, sample_time=None,
                        output_limits=(-self.max_acceleration, self.max_acceleration))
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup([dir_pin, enable_pin], GPIO.OUT)
-        # self.home()
 
     def _worker(self):
         # Continuously check for jobs in the queue
@@ -149,12 +135,12 @@ class Stepper():
         pass
 
     def zero(self):
-        # self.position = 0
         pass
 
     def home(self):
-        self.do_steps_sync(int(self.steps_per_rev / 4))
-        self.do_steps_sync(-int(self.steps_per_rev / 4))
+        pass
+        # self.do_steps_sync(int(self.steps_per_rev / 4))
+        # self.do_steps_sync(-int(self.steps_per_rev / 4))
 
     def stop(self):
         logger.debug("stopping motor")
@@ -169,14 +155,15 @@ class Stepper():
         GPIO.cleanup([self.enable_pin, self.dir_pin])
 
     def _set_speed(self, velocity: float):
-        """Set the step pin to pulse at the specified frequency."""
         if self.pwm == None:
             logger.error("No pwm configured!")
             return
+
         logger.debug(f"Setting pwm: {velocity}")
         GPIO.output(self.enable_pin, GPIO.HIGH)
+
         if velocity == 0:
-            self.pwm.stop()  # Stop the motor
+            self.pwm.stop()
         elif velocity > 1:
             GPIO.output(self.dir_pin, GPIO.LOW)
             self.pwm.start(50)
@@ -186,7 +173,7 @@ class Stepper():
             self.pwm.start(50)
             self.pwm.change_frequency(-velocity)
         else:
-            self.pwm.stop()  # Stop the motor
+            self.pwm.stop()
 
     def do_steps_sync(self, *args):
         self.job_queue.put(args)
@@ -200,13 +187,11 @@ class Stepper():
 
         logger.debug(f"Doing {step_count} steps. Will take {duration} seconds")
         GPIO.output(self.enable_pin, GPIO.HIGH)
-        # self.position += step_count
+
         if step_count > 0:
             velocity = abs(velocity)
-            # GPIO.output(self.dir_pin, GPIO.LOW)
         else:
             velocity = -abs(velocity)
-            # GPIO.output(self.dir_pin, GPIO.HIGH)
 
         self._set_speed(velocity)
         time.sleep(duration)
@@ -271,7 +256,7 @@ class Stepper():
         self._last_time = now
 
         # update dynamics
-        self.acceleration = self.pid(self.distance)
+        self.acceleration = self.pid(self.distance)  # pid calculation
         self.velocity += self.acceleration * dt
         self.velocity = max(-self.max_velocity,
                             min(self.max_velocity, self.velocity))
@@ -279,33 +264,9 @@ class Stepper():
                      f"dt: {dt}")
         self._set_speed(self.velocity)
 
-        # (p, i, d) = self.pid.components
-        # (yaw, roll, pitch) = self.sensor.euler
-        # sys, gyro, accel, mag = self.sensor.calibration_status
-
-        # logger.debug(
-        #     f"{(UP+CLR)*18}"
-        #     f"{'-'*40}\n"
-        #     f"dt: {dt:.4f}\n"
-        #     f"p: {p:.4f}\n"
-        #     f"i: {i:.4f}\n"
-        #     f"d: {d:.4f}\n"
-        #     f"a: {self.acceleration:.4f}\n"
-        #     f"v: {self.velocity:.4f}\n"
-        #     # f"yaw: {yaw:.0f}\n"
-        #     # f"pitch: {pitch:.0f}\n"
-        #     # f"roll: {roll:.0f}\n"
-        #     # f"sys: {sys:.0f}\n"
-        #     # f"gyro: {gyro:.0f}\n"
-        #     # f"accel: {accel:.0f}\n"
-        #     # f"mag: {mag:.0f}\n"
-        #     f"goal: {self.goal:.0f}\n"
-        #     f"distance: {self.distance:.0f}\n"
-        #     f"out of: {ControlledStepper.max_delay}"
-        # )
-
+        # Run again after pid_delay
         timer = threading.Timer(
             self.pid_delay,
             self._calc_pid)
-        timer.daemon = True  # Makes sure the timer stops when the process crashes
+        timer.daemon = True  # Makes sure the timer stops when the process ends/crashes
         timer.start()
